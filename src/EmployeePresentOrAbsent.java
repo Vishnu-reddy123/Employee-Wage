0cd0671526bd822906ede5cb636deaea20b86212
Vishnu-reddy123 <vishnu95532@gmail.com>
2022-10-11 10:28:13 +0530

    first commit

---
+++ src/EmployeePresentOrAbsent.java
@@ -0,0 +1,12 @@
+public class EmployeePresentOrAbsent {
+    public static void main(String[] args){
+        double random = Math.random();
+        System.out.println(random);
+        if(random>0.5) {
+            System.out.println("Employee is present");
+        }
+        else {
+            System.out.println("Employee is absent");
+        }
+    }
+}
---
+++ src/PartTime.java
@@ -0,0 +1,22 @@
+public class PartTime {
+            public static void main(String[] args){
+                int isPartTime = 1;
+                int isFullTime = 2;
+                int empRatePerHr = 20;
+                int empHrs = 0;
+                double empCheck = Math.floor(Math.random() * 10) % 3;
+                if(isPartTime == empCheck) {
+                    empHrs = 8;
+                }
+                else if(isFullTime == empCheck) {
+                    empHrs = 16;
+                }
+                else{
+                    empHrs = 0;
+                }
+                int salary = (empHrs * empRatePerHr);
+                System.out.println("Employee salary is:"+salary);
+            }
+        }
+
+
